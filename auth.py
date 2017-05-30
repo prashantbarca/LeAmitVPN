d045f568c97dee8f8cfa90453be9e0791409e92a
prashant <prashant.barca@gmail.com>
2017-05-29 21:30:24 -0400

    Add more instructions to auth.py

--- auth.py
+++ auth.py
@@ -6,6 +6,8 @@ import os
 import pytap
 import md5
 
+STATES = ["Closed", "Authenticated"]
+current_states = {"10.10.0.2": 0, "10.10.0.3": 0}
 SERVER_UDP_PORT = 5050
 SERVER_UDP_IP = "128.199.177.106"
 
@@ -13,7 +15,7 @@ users = {"10.10.0.2": md5.new("pw1").digest(), "10.10.0.3": md5.new("pw2").diges
 addresses = {"10.10.0.2": None, "10.10.0.3": None}
 
 def validate_user(username, pw):
-    if users[username] == md5.new(pw).digest():
+    if users[username] == pw:
         return True
     else:
         return False
@@ -22,13 +24,22 @@ def send_auth_packet(sock, username, pw):
     sock.sendto("username:"+username+":"+md5.new(pw).digest(), (SERVER_UDP_IP, 5050))
     return
 
-def recv_auth(sock, message):    
-    username = message
-    pw = message
+def recv_auth(sock, addr, message):
+    username = message.split(':')[1]
+    pw = message.split(':')[2]
     if validate_user(username, pw):
+        sock.sendto("Authenticated", addr)
+        addresses[username] = addr
+        current_states[username] = 1
         return True
     else:
         return False
     return
+
+def recv_disconnect(sock, addr, username, pw):
+    if validate_user(user, pw):
+        sock.sendto("Disconnected", addr)
+        addresses[username] = None
+        current_states[username] = 0
 
 
