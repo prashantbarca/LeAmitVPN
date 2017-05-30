11db9a9aba6d8a9f7df260a8e7daa32e42e21ce5
Prashant <prashant.barca@gmail.com>
2017-05-30 17:04:35 -0400

    dos2unix fixed

--- amitcrypto.py
+++ amitcrypto.py
@@ -1,10 +1,29 @@
-
+import base64
+import hashlib
+from Crypto import Random
 from Crypto.Cipher import AES
 
-aesobj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+class AESCipher(object):
 
-def encrypt(sock, message, addr):
-    sock.sendto(aesobj.encrypt(message), addr)
+    def __init__(self, key):
+        self.bs = 32
+        self.key = hashlib.sha256(key.encode()).digest()
 
-def decrypt(sock, message, addr):
-    return aesobj.decrypt(message)
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
+
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs) ##instead of self.bs: block_size
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
